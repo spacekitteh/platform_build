80b25afcf7876cf7a09a29050187ecf7fdf847b9
Daniel Micay <danielmicay@gmail.com>
2016-01-06 16:10:54 -0500

    add Updater app

--- target/product/core.mk
+++ target/product/core.mk
@@ -56,6 +56,7 @@ PRODUCT_PACKAGES += \
     TeleService \
     VpnDialogs \
     MmsService \
-    chromium
+    chromium \
+    Updater
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_base.mk)
