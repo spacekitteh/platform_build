9be461b8cba202a2a8956ee0447a47f2bc9a1eff
Daniel Micay <danielmicay@gmail.com>
2016-09-14 09:42:40 -0400

    disable bounds sanitizer for icu
    Change-Id: I6c1c7f8551878aef278bbcd195ab22e7b183d1bb

--- core/config_sanitizers.mk
+++ core/config_sanitizers.mk
@@ -149,8 +149,12 @@ ifeq ($(filter undefined,$(my_sanitize)),)
   endif
 endif
 
+BOUNDS_BLACKLIST := libicuuc libicuuc_static libicui18n libicui18n_static
+
 ifndef LOCAL_IS_HOST_MODULE
-  ifeq ($(my_clang),true)
-    my_cflags += -fsanitize=bounds -fsanitize-trap=bounds
+  ifeq ($(filter $(LOCAL_MODULE),$(BOUNDS_BLACKLIST)),)
+    ifeq ($(my_clang),true)
+      my_cflags += -fsanitize=bounds -fsanitize-trap=bounds
+    endif
   endif
 endif
